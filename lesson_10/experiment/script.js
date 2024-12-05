2f2ec8a61e08f8d365d6d229f7500045c6a34cb0
LutsDM <lutsdm@gmail.com>
2024-12-06 00:21:58 +0100

    game

--- lesson_10/experiment/script.js
+++ lesson_10/experiment/script.js
@@ -1,8 +1,61 @@
-import btn from 'btn';
-const btn1 = document.getElementById("button1");
-const clonedBtn = btn.cloneNode(true);
-clonedBtn.id = "button2";
+const redBtn = document.querySelector("#red-button");
+const btn1 = document.querySelector("#button1");
+const title = document.querySelector(".title");
 
-document.getElementById("button1").addEventListener("click", function() {
-    document.title = "Новый заголовок";
+redBtn.addEventListener("click", () => {
+  redBtn.remove(); //удаляем предыдущую кнопку
+
+  title.innerText = 'Мне кажется там было понятно написанно "Не нажимать"'; //меняем лейбл подпись над кнопкой
+
+  let btn1 = document.createElement("button"); //создаем новую кнопку
+
+  btn1.id = "button1"; //назначаем ей заготовленный стиль из css
+
+  document.querySelector(".button-container").append(btn1); // помещаем в контейнер для кнопок
+
+  btn1.addEventListener("click", () => {
+    //повторяем
+    btn1.remove();
+    title.innerText = "Это уже хамство";
+
+    let btn2 = document.createElement("button");
+
+    btn2.id = "button2";
+
+    document.querySelector(".button-container").append(btn2);
+
+    btn2.addEventListener("click", () => {
+      btn2.addEventListener("click", () => {
+        btn2.addEventListener("click", () => {
+          btn2.addEventListener("click", () => {
+            btn2.remove();
+            title.innerText = "А ты настойчивый";
+
+            let btn3 = document.createElement("button");
+
+            btn3.id = "button3";
+
+            document.querySelector(".button-container").append(btn3);
+
+            btn3.addEventListener("click", () => {
+                btn3.remove();
+                title.innerText = "А если так?";
+    
+                let btn4 = document.createElement("button");
+    
+                btn4.id = "button4";
+    
+                document.querySelector(".button-container").append(btn4);
+
+
+
+
+
+
+
+          });
+        });
+      });
+    });
+  });
 });
