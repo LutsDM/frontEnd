6ef9b007bbf38c2a86c0a2fadb00bea0f4cef7d9
Dmytro Luts <151907755+LutsDM@users.noreply.github.com>
2024-12-05 09:32:24 +0000

    rr

--- lesson_10/homework10/index.js
+++ lesson_10/homework10/index.js
@@ -4,18 +4,20 @@ const btn = document.getElementById("magic-btn");
 const clonedBtn = btn.cloneNode(true);
 // задали id
 clonedBtn.id = "magic-btn-2";
+clonedBtn.innerText = "Я изменю тебя";
+clonedBtn.style.backgroundColor = "#a78b71";
+clonedBtn.style.color = "white";
+clonedBtn.className = "second-btn";
 
-btn.addEventListener('click', () => {
+btn.addEventListener("click", () => {
   // при нажатии на первую кнопку
   // работайте с clonedBtn
   // ваш код начинается здесь
   document.body.appendChild(clonedBtn);
-  clonedBtn.innerText = "Я изменю тебя";
-  clonedBtn.style.backgroundColor =  "#a78b71"; 
-  clonedBtn.style.color = "white";
-  btn.style.backgroundColor = "#9c4a1a";
-  btn.style.color = "black";
-  
-})
+
+clonedBtn.addEventListener('click', () => {
+btn.style.backgroundColor = "#9c4a1a";
+btn.style.color = "black";
+});
 
 // здесь можете создать EventListener для второй кнопки
