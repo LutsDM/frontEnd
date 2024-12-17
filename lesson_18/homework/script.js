7a03e404f31d112f7a9c7a5333d1be81f6dd48cf
lutsd <lutsdm@gmail.com>
2024-12-17 22:15:44 +0100

    wew

--- lesson_18/homework/script.js
+++ lesson_18/homework/script.js
@@ -2,8 +2,6 @@ async function product() {
     const res = await fetch("https://dummyjson.com/products");
     const data = await res.json();
 
-    data.map(product => {
-    //создание карточки
-    cons
-    
-}
+    // data.map(product => {
+    // //создание карточки
+    // cons
