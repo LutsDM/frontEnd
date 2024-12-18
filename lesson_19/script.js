2582e246312ff6c2b9ec5659a3f168c8b24206f7
LutsDM <lutsdm@gmail.com>
2024-12-18 12:07:26 +0100

    weather

--- lesson_19/script.js
+++ lesson_19/script.js
@@ -1,16 +1,25 @@
-const weatherApi = document.querySelector('#weather') 
+const weatherApi = document.querySelector("#weather");
 
-async function weatherForecast() {
-    const res = await fetch("https://get.geojs.io/v1/ip/geo.json");
-    const data = await res.json();
-   
-    // const latitude = data.latitude;
-    // const longitude = data.longitude;
-    // const city = data.city;
+async function myLocation() {
+  const res = await fetch("https://get.geojs.io/v1/ip/geo.json");
+  const data = await res.json();
 
-    const {city, latitude, longitude} = data 
-   
-    console.log(latitude, longitude, city)
+  // const latitude = data.latitude;
+  // const longitude = data.longitude;
+  // const city = data.city;
+
+  const { city, latitude, longitude } = data;
+
+  console.log(latitude, longitude, city);
+
+    const weatherRes = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true`);
+    const weatherData = await weatherRes.json();
+  
+    const { temperature, windspeed, weathercode} = weatherData.current_weather;
+    const { temperature:temperatureUnit, windspeed:windspeedUnit} = weatherData.current_weather_units
+  
+    console.log(temperature, windspeed, weathercode, temperatureUnit, windspeedUnit);
+    weatherApi.append (`${city}, Current temperature: ${temperature} ${temperatureUnit}, Windspeed: ${windspeed} ${windspeedUnit}`);
 }
 
-weatherForecast()
+myLocation();
